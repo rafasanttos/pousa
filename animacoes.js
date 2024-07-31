5a679874addce1a907b068fc92709a81a5642b94
RAFAEL DOS SANTOS <79388000+rafasanttos@users.noreply.github.com>
2024-07-31 10:35:58 -0300

    Add files via upload

---
+++ animacoes.js
@@ -0,0 +1,101 @@
+ScrollReveal().reveal('.texto-topao h1' ,{
+    scale: 2,
+    duration: 2000,
+});
+// ScrollReveal().reveal('.apresentacao' ,{
+//     distance: '50px',
+//     duration: 2000,
+// });
+// ScrollReveal().reveal('.container-falando-alter' ,{
+//     distance: '50px',
+//     duration: 2000,
+// });
+// ScrollReveal().reveal('.sugestaoh2' ,{
+//     distance: '50px',
+//     duration: 2000,
+// });
+// ScrollReveal().reveal('.sugestao-passeio' ,{
+//     distance: '50px',
+//     duration: 2000,
+// });
+// ScrollReveal().reveal('.faq' ,{
+//     distance: '50px',
+//     duration: 2000,
+// });
+
+
+// ScrollReveal().reveal('.texto-topao p' ,{
+//     delay:1000,
+//     distance:'90px',
+//     duration:1000,
+
+
+// });
+
+// ScrollReveal().reveal('.inicial-img' ,{
+
+//     opacity : 0.2,
+//     duration:1000,
+
+
+// });
+
+// ScrollReveal().reveal('.efeito-apresentacao h2',{
+
+//     distance:'50px',
+//     duration:1500,
+
+
+// });
+
+// ScrollReveal().reveal('.efeito-apresentacao p',{
+//     delay: 300,
+//     distance:'50px',
+//     duration:1500,
+
+
+// });
+
+// ScrollReveal().reveal('.efeito-sobre-primeira-caixa',{
+//     duration: 2000,
+//     distance: '300px',
+//     origin:'left',
+    
+    
+
+
+// });
+
+// ScrollReveal().reveal('.efeito-sobre-segunda-caixa',{
+//     delay: 700,
+//     duration: 2000,
+//     distance: '100px',
+//     origin:'top',
+    
+    
+
+
+// });
+
+// ScrollReveal().reveal('.efeito-sobre-terceira-caixa',{
+//     delay: 1000,
+//     duration: 2000,
+//     distance: '-300px',
+//     origin:'left',
+    
+    
+
+
+// });
+
+// ScrollReveal().reveal('.botao-final',{
+//     delay: 400,
+//     duration: 2000,
+//     scale:3
+//     // distance: '90px',
+//     // origin:'bottom',
+    
+    
+
+
+// });
